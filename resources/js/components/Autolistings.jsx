5fbbfe30a17681c53fae31cbb365e77777047780
Tey <justinevillanueva090794@gmail.com>
2022-09-18 18:13:11 +0800

    Navbar added

---
+++ resources/js/components/Autolistings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Autolistings() {
+  return (
+ 
+  <div>Autolistings Page</div>
+  
+  );
+}
---
+++ resources/js/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import Logo from '../../media/jusLogoImg.png';
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
+  return (
+    <>
+      <nav className='navbar sticky-top d-flex justify-content-between'>
+        <a href='/' onClick={closeMobileMenu}>
+            <img className='navbar-logo' src={Logo} alt='Rental Car'/>
+          
+        </a>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        
+          <li className='nav-item'>
+            <a href='/#'
+              className='nav-links'
+            >
+              HOME
+            </a>
+          </li>
+          
+          <li className='nav-item'>
+            <a href='/Autolistings/#'
+              className='nav-links'
+            >
+              AUTOLISTINGS
+            </a>
+          </li>
+         
+          <li className='nav-item'>
+            <a href='/#indexaboutus'
+              className='nav-links'
+            >
+              ABOUT&#160;US
+            </a>
+          </li>
+
+          <li className='nav-item'>
+            <a href='/#testimonials'
+              className='nav-links'
+            >
+              TESTIMONIALS
+            </a>
+          </li>
+
+          <li className='nav-item'>
+            <a href='/News/#'
+              className='nav-links'
+            >
+              NEWS
+            </a>
+          </li>
+
+          <li className='nav-item'>
+            <a href='/Contact/#'
+              className='nav-links'
+            >
+             CONTACT&#160;US
+            </a>
+          </li>
+
+        </ul>
+       
+      </nav>
+    </>
+  );
+}
+
+export default Navbar
